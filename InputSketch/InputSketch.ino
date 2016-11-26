27ed9bfce4385952d5436c21a6331244688d0330
caicai67 <caitlinmorris@Caitlins-MacBook-Pro.local>
2016-11-26 16:31:07 -0500

    1 added arduino sketch, 2 changed project settings, 3 added gitignore
    1 - made a very basic sketch with a baud speed matching that of the
    serialport instantiated in unity
    2 - added support for entire .NET framework (which deals with ports)
    from project settings -> player -> other -> optimization
    3 added git ignore. there may still be some troublesome files that need
    to be untracked, but we’ll find that out when you push for the first
    time

---
+++ InputSketch/InputSketch.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
